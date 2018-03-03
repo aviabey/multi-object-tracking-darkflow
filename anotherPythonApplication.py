f1ddf5de036b628f9cd53223467103b997c7d770
Avi <aviabey@gmail.com>
2018-03-04 01:38:45 +0530

    tracker refresh added

--- anotherPythonApplication.py
+++ anotherPythonApplication.py
@@ -2,7 +2,7 @@ from darkflow.net.build import TFNet
 import cv2
 import sys
 
-options = {"model": "cfg/yolo-obj.cfg", "load": "bin/yolo-obj_300.weights", "threshold": 0.6}
+options = {"model": "cfg/yolo-obj.cfg", "load": "bin/yolo-obj_300.weights", "threshold": 0.72, "saveVideo":""}
 
 tfnet = TFNet(options)
 
@@ -30,7 +30,7 @@ is_first_frame = 1
 tracking_list = []  # [(x1y1),(x2,y2)] - stores the mid points given from tracker
 new_detection = 0
 current_detections_on_d_frame = 0
-frame_count=0
+frame_count = 1
 
 while True:
     # Read a new frame
@@ -50,15 +50,14 @@ while True:
         bot = one_detection['bottomright']['y']
         mid_x = left + (right - left) / 2
         mid_y = top + (bot - top) / 2
-        cv2.rectangle(frame, (left, top), (right, bot), (0, 255, 0), 2, 1)
-        if mid_y > 100 and mid_y<600:  # safe upper margin
+        cv2.rectangle(frame, (left, top), (right, bot), (0, 255, 0), 3, 1)
+        if mid_y > 100 and mid_y < 580:  # safe upper margin
             # current_frame_detection_id +=1
             same_detection_has_found = 0
             print("-----current_frame_detection_id ------", current_frame_detection_id)
 
             if is_first_frame == 1:
                 new_detection = 1
-                yield_object_total_count += 1
             if is_first_frame == 0:
                 for index in range(len(tracking_list)):
                     # print('---11111111111-------')
@@ -88,28 +87,46 @@ while True:
     print("len(bounding_boxes_for_tracker)", len(bounding_boxes_for_tracker))
     print(bounding_boxes_for_tracker)
 
-
     if len(same_detections_this_frame) > 0:  # has previous detections
         for index1 in sorted(same_detections_this_frame, reverse=True):
-            del bounding_boxes_for_tracker[index1] # removing previous detected boxes
+            del bounding_boxes_for_tracker[index1]  # removing previous detected boxes
     print("len(bounding_boxes_for_tracker) after", len(bounding_boxes_for_tracker))
 
-    if len(bounding_boxes_for_tracker) > 0:  # new bboxes to add
+    if len(bounding_boxes_for_tracker) > 0:  # has new bboxes to add
         new_detection = 1
-        yield_object_total_count += 1
 
     if ok:
         if not init_once:
             if new_detection == 1:
                 for index in range(len(bounding_boxes_for_tracker)):
                     ok = tracker.add(cv2.TrackerMIL_create(), frame, bounding_boxes_for_tracker[index])
-                # ok = tracker.add(cv2.TrackerMIL_create(), frame, bbox2)
+                    yield_object_total_count += 1
                 # init_once = True
                 new_detection = 0
 
         # Update tracker
         ok, boxes = tracker.update(frame)
         print(ok, boxes)
+
+        # reset main tracker on each and every 20 frames
+        # if frame_count  == 0:
+        if frame_count % 20 == 0:
+            print("tracker reset")
+            keeping_boxes=[]
+            tracker = cv2.MultiTracker_create()
+            for one_box in boxes:
+                print(one_box)
+                # print(boxes[index2][0])
+                # print(boxes[index2][2])
+                if one_box[1] + one_box[3]/2 < 600:
+                    bbox_to_add = (one_box[0],
+                                   one_box[1],
+                                   one_box[2],
+                                   one_box[3])
+                    ok = tracker.add(cv2.TrackerMIL_create(), frame, bbox_to_add)
+                    keeping_boxes.append(one_box)
+            boxes=keeping_boxes
+            print("filtered boxes",boxes)
 
         tracking_list = []
         # Draw bounding box
@@ -131,11 +148,19 @@ while True:
     # fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
 
     # Display FPS on frame
-    frame_count+= 1
-    cv2.putText(frame, "Frame No : " + str(int(frame_count)), (100, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+    cv2.putText(frame, "Frame No : " + str(int(frame_count)), (100, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50),
+                2)
     # cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
-    cv2.putText(frame, "Count : " + str(int(yield_object_total_count)), (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2);
+    cv2.putText(frame, "Count : " + str(int(yield_object_total_count))+"/ Weight : " + str(float(yield_object_total_count*0.123))+"kg", (100, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+                (0, 0, 0), 2);
     # medium size tomato weight 123g
+
+    # Draw margins
+    cv2.rectangle(frame, (25, 100), (1250, 600), (2, 2, 253), 1, 1)
+
+    print("frame_count", frame_count)
+    frame_count += 1
 
     # Display result
     cv2.imshow("Tracking", frame)
